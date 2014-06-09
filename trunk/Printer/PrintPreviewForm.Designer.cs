e112ed5703a4f9813272b7833133097250996e64
sam.theisens@gmail.com <sam.theisens@gmail.com@f2595722-1d14-11df-8c1f-719d98f73570>
2014-06-09 14:46:40 +0000

    Working towards being able to verify form designs and print without going via medismart executable  * Added x86 target, because crystal reports seems to work only in 32bit mode  * creating in memory dataset, because directly binding to OleDBDatasetReader does not work

--- trunk/Printer/PrintPreviewForm.Designer.cs
+++ trunk/Printer/PrintPreviewForm.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrintPreviewForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.previewButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.spParameter = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -46,7 +47,7 @@
             // 
             this.panel1.Controls.Add(this.previewButton);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.spParameter);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 16);
             this.panel1.Name = "panel1";
@@ -72,12 +73,13 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Stored procedure Parameter";
             // 
-            // textBox1
+            // spParameter
             // 
-            this.textBox1.Location = new System.Drawing.Point(149, 8);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.spParameter.Location = new System.Drawing.Point(149, 8);
+            this.spParameter.Name = "spParameter";
+            this.spParameter.Size = new System.Drawing.Size(100, 20);
+            this.spParameter.TabIndex = 0;
+            this.spParameter.Text = "0015520-06";
             // 
             // groupBox1
             // 
@@ -96,7 +98,7 @@
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.reportViewer);
+            this.groupBox2.Controls.Add(this.crystalReportViewer1);
             this.groupBox2.Location = new System.Drawing.Point(6, 75);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(834, 437);
@@ -104,13 +106,16 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Preview";
             // 
-            // reportViewer
+            // crystalReportViewer1
             // 
-            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.reportViewer.Location = new System.Drawing.Point(3, 16);
-            this.reportViewer.Name = "reportViewer";
-            this.reportViewer.Size = new System.Drawing.Size(828, 418);
-            this.reportViewer.TabIndex = 0;
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(3, 16);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(828, 418);
+            this.crystalReportViewer1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -128,6 +133,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(855, 515);
             this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PrintPreviewForm";
             this.Text = "PrintPreviewForm";
             this.Load += new System.EventHandler(this.PrintPreviewForm_Load);
@@ -143,12 +149,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox spParameter;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button previewButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
         private System.Windows.Forms.Panel panel2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
     }
 }
