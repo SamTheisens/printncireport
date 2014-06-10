aad3ef210f2ecf79c2c596a3c67ed6cdd215b02e
sam.theisens@gmail.com <sam.theisens@gmail.com@f2595722-1d14-11df-8c1f-719d98f73570>
2014-06-10 18:47:24 +0000

    Working on being able to skip Print_Report.exe  * added print button to preview form  * added PrintLangsung setting (false by default)  * created ReportService for loading and printing the report  * renamed eventhandlers

---
+++ trunk/Printer/Forms/PrintPreviewForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+using Printer.Services;
+
+namespace Printer.Forms
+{
+    public partial class PrintPreviewForm : Form
+    {
+        private readonly ReportService _reportService;
+        private Report _report;
+
+        public PrintPreviewForm()
+        {
+            InitializeComponent();
+        }
+
+        public PrintPreviewForm(Report report)
+        {
+            _report = report;
+            _reportService = new ReportService();
+
+            InitializeComponent();
+        }
+
+        private void PrintPreviewFormLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PreviewButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                crystalReportViewer1.ReportSource = _reportService.CreateReport(_report);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format(CultureInfo.CurrentCulture, ex.Message));
+            }
+        }
+
+        private void PrintButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+
+             _reportService.PrintReport(_report);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format(CultureInfo.CurrentCulture, ex.Message));
+            }
+        }
+
+        private void SpParameterTextChanged(object sender, EventArgs e)
+        {
+            _report.Parameter = spParameter.Text;
+        }
+
+        
+
+
+    }
+}
